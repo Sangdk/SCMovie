14e864326678e0e0170ee4cdf56c80ab1cde865a
Linhchi902 <Chibaby9297@gmail.com>
2019-09-13 10:12:46 +0700

    add screen splash

---
+++ app/src/main/java/com/t3h/scmovie/activity/SplashActivity.java
@@ -0,0 +1,50 @@
+package com.t3h.scmovie.activity;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.view.Gravity;
+import android.view.Menu;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import androidx.transition.ArcMotion;
+import androidx.transition.ChangeBounds;
+import androidx.transition.TransitionManager;
+
+import com.t3h.scmovie.R;
+import com.t3h.scmovie.base.BaseActivity;
+
+public class SplashActivity  extends BaseActivity {
+    private ProgressBar prg;
+
+    @Override
+    protected void initAct() {
+        prg = findViewById(R.id.prg_bar);
+        Animation animation = AnimationUtils.loadAnimation(SplashActivity.this,R.anim.translate);
+        ImageView img = findViewById(R.id.img_logo);
+        animation.reset();
+        img.clearAnimation();
+        img.startAnimation(animation);
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+
+
+                /* Create an Intent that will start the Menu-Activity. */
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
+
+            }
+        }, 3000);
+    }
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_splash;
+    }
+}
